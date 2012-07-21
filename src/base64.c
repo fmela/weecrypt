423e564b77422788819e416c42415214cf56b47d
Farooq Mela <farooq.mela@gmail.com>
2012-07-20 17:22:04 -0700

    Fix bugs and update base64 decode interface.

--- src/base64.c
+++ src/base64.c
@@ -34,111 +34,135 @@ static const unsigned char base64_chars[] =
 
 #define BASE64_PAD		'='
 
-void *
+static unsigned char decode(unsigned char code) {
+	if (code >= 'A' && code <= 'Z') {
+		return code - 'A';
+	} else if (code >= 'a' && code <= 'z') {
+		return code - 'a' + 26;
+	} else if (code >= '0' && code <= '9') {
+		return code - '0' + 52;
+	} else if (code == '+') {
+		return 62;
+	} else if (code == '/') {
+		return 63;
+	} else {
+		return 0xFF;
+	}
+}
+
+char *
 base64_encode(const void *input, unsigned input_size, unsigned *output_size)
 {
-	const unsigned char *in;
-	unsigned char *outbuf, *code;
-	unsigned pos, outbuf_size, pad, line = 0;
+	if (!input || !input_size) {
+		return "";
+	}
+	const unsigned char *in = (const unsigned char *)input;
+	const unsigned pad = input_size % 3;
+	unsigned outbuf_size = 4 * ((input_size + 2) / 3);
+	/* Add two bytes for every 72 characters, plus one for the trailing '\0'. */
+	outbuf_size += 2 * ((outbuf_size - 1) / 72) + 1;
+	unsigned char *outbuf = MALLOC(outbuf_size);
 
-	ASSERT(input != NULL);
-	ASSERT(input_size > 0);
-
-	in = (const unsigned char *)input;
-	pad = input_size % 3;
-	outbuf_size = (input_size * 4 + 2) / 3;
-	outbuf_size += (outbuf_size / 36) + 2;
-	if (pad)
-		outbuf_size++;
-	outbuf_size += 2;
-	outbuf = MALLOC(outbuf_size);
-
-	for (code = outbuf, pos = 0; pos + 3 <= input_size; in += 3, pos += 3) {
-		*code++ = base64_chars[((in[0] >> 2) & 0x3f)];
-		*code++ = base64_chars[((in[0] << 4) & 0x30) | ((in[1] >> 4) & 0xf)];
-		*code++ = base64_chars[((in[1] << 2) & 0x3c) | ((in[2] >> 6) & 0x3)];
-		*code++ = base64_chars[((in[2] >> 0) & 0x3f)];
-
-		if ((line += 4) >= 72) {
+	unsigned char *code = outbuf;
+	unsigned line = 0;
+	for (unsigned pos = 0; pos + 3 <= input_size; in += 3, pos += 3) {
+		if (line >= 72) {
 			*code++ = '\r';
 			*code++ = '\n';
 			line = 0;
 		}
+		*code++ = base64_chars[((in[0] >> 2) & 0x3f)];
+		*code++ = base64_chars[((in[0] << 4) & 0x30) | ((in[1] >> 4) & 0xf)];
+		*code++ = base64_chars[((in[1] << 2) & 0x3c) | ((in[2] >> 6) & 0x3)];
+		*code++ = base64_chars[((in[2] >> 0) & 0x3f)];
+		line += 4;
 	}
 
 	if (pad) {
+		if (line >= 72) {
+			*code++ = '\r';
+			*code++ = '\n';
+			line = 0;
+		}
 		*code++ = base64_chars[((in[0] >> 2) & 0x3f)];
-		if (pad > 1) {
-			*code++ = base64_chars[((in[0] << 4) & 0x30)|((in[1] >> 4) & 0xf)];
+		if (pad == 2) {
+			*code++ = base64_chars[((in[0] << 4) & 0x30) |
+				                   ((in[1] >> 4) & 0xf)];
 			*code++ = base64_chars[((in[1] << 2) & 0x3c)];
 		} else {
+			ASSERT(pad == 1);
 			*code++ = base64_chars[(in[0] << 4) & 0x30];
 			*code++ = BASE64_PAD;
 		}
 		*code++ = BASE64_PAD;
 	}
 
-	*code++ = '\r';
-	*code++ = '\n';
-	*code++ = '\0';
 	if (output_size != NULL)
 		*output_size = code - outbuf;
+	*code++ = '\0';
 
-	if ((unsigned)(code - outbuf) > outbuf_size)
-		printf("%u: Allocated %u, wrote %u\n", input_size, outbuf_size, code - outbuf);
+	if (outbuf_size != code - outbuf) {
+		printf("encoding %u bytes: allocated %u, wrote %ld\n",
+			   input_size, outbuf_size, code - outbuf);
+		printf("encoded: \"%s\"\n", outbuf);
+	}
 
-	return outbuf;
+	return (char *)outbuf;
 }
 
 void *
-base64_decode(const void *input, unsigned input_size, unsigned *output_size)
+base64_decode(const char *input, unsigned *output_size)
 {
-	const unsigned char *p, *in;
-	unsigned char *outbuf, *code;
-	unsigned int data[4];
-	int i;
-	unsigned pos = 0, outbuf_size;
-
 	ASSERT(input != NULL);
-	ASSERT(input_size != 0);
 
-	outbuf_size = ((input_size * 3) / 4) + 2;
-	code = outbuf = MALLOC(outbuf_size);
-	in = (const unsigned char *)input;
-
-	while (pos + 3 < input_size) {
-		for (i = 0; i < 4; i++) {
-			for (p = base64_chars; *p; p++)
-				if (*p == in[pos])
-					break;
-			pos++;
-			if (*p)
-				data[i] = p - base64_chars;
-			else
-				i--;
+	unsigned input_size = 0;
+	unsigned base64_bytes = 0;
+	for (; input[input_size]; ++input_size) {
+		if (decode(input[input_size]) < 64) {
+			++base64_bytes;
 		}
+	}
+	if (input_size == 0) {	/* Special handling of empty string. */
+		if (output_size) {
+			*output_size = 0;
+		}
+		return MALLOC(1);
+	}
+	unsigned outbuf_size = (base64_bytes * 3) / 4;
+	unsigned char *outbuf = MALLOC(outbuf_size);
+	const unsigned char *in = (const unsigned char *)input;
 
-		code[0] = ((data[0] << 2) & 0xfc) | ((data[1] >> 4) & 0x03);
-		code[1] = ((data[1] << 4) & 0xf0) | ((data[2] >> 2) & 0x0f);
-		code[2] = ((data[2] << 6) & 0xc0) | ((data[3] >> 0) & 0x3f);
-
-		if (data[3] == 64) {
-			if (data[2] == 64) {
-				code[1] = 0;
-				code -= 2;
-			} else {
-				code[2] = 0;
-				code--;
+	unsigned char *code = outbuf;
+	unsigned char data[4];
+	unsigned int ndata = 0;
+	for (unsigned pos = 0; pos < input_size; ++pos) {
+		unsigned char decode_char = decode(in[pos]);
+		if (decode_char < 64) {
+			data[ndata] = decode_char;
+			if (++ndata == 4) {
+				*code++ = ((data[0] << 2) & 0xfc) | ((data[1] >> 4) & 0x03);
+				*code++ = ((data[1] << 4) & 0xf0) | ((data[2] >> 2) & 0x0f);
+				*code++ = ((data[2] << 6) & 0xc0) | ((data[3] >> 0) & 0x3f);
+				ndata = 0;
 			}
 		}
-		code += 3;
+	}
+	if (ndata >= 2) {
+		*code++ = ((data[0] << 2) & 0xfc) | ((data[1] >> 4) & 0x03);
+		if (ndata >= 3) {
+			*code++ = ((data[1] << 4) & 0xf0) | ((data[2] >> 2) & 0x0f);
+		}
+	} else if (ndata != 0) {
+		printf("%d(s) stray input bytes.\n", ndata);
 	}
 
 	if (output_size != NULL)
 		*output_size = code - outbuf;
 
-	if ((unsigned)(code - outbuf) > outbuf_size)
-		printf("%u: Allocated %u, wrote %u\n", input_size, outbuf_size, code - outbuf);
+	if (outbuf_size != code - outbuf) {
+		printf("decoding %u bytes: allocated %u, wrote %ld\n",
+			   input_size, outbuf_size, code - outbuf);
+	}
 
 	return outbuf;
 }
