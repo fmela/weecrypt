8b10b347b4895deeaee86c4c584327432ac102a0
Farooq Mela <farooq.mela@gmail.com>
2012-07-20 17:22:54 -0700

    Pull factoring into factor() routine.

--- prime.c
+++ prime.c
@@ -4,13 +4,13 @@
 
 #include "weecrypt.h"
 
+void factor(mp_digit *n, mp_size len);
+
 int
 main(void)
 {
 	mp_digit *n;
-	mp_digit factor, remainder;
 	mp_size len;
-	int nfactors, npowers;
 	char buf[8192];
 
 	for (;;) {
@@ -29,35 +29,41 @@ main(void)
 		mp_print_dec(n, len);
 		printf("\n");
 
-		nfactors = 0;
-		while ((factor = mp_sieve(n, len, 0)) != 0) {
-			nfactors++;
-			npowers = 0;
-			do {
-				remainder = mp_ddivi(n, len, factor);
-				ASSERT(remainder == 0);
-				len -= (n[len-1] == 0);
-				npowers++;
-			} while (mp_dmod(n, len, factor) == 0);
-			if (npowers > 1)
-				printf("Trivial factor %u^%d removed\n", factor, npowers);
-			else
-				printf("Trivial factor %u removed\n", factor);
-		}
-
-		printf("Remainder after removing %d trivial factors is:\n", nfactors);
-		mp_print_dec(n, len); printf("\n");
-
-		if (len == 1 && n[0] <= 2) {
-			printf("Trivial remainder\n");
-		} else {
-			/* Now run 10 rounds of the Rabin-Miller test. */
-			if (mp_composite(n, len, 10))
-				printf("Remainder is definitely composite.\n");
-			else
-				printf("Remainder is prime with extreme probability.\n");
-		}
+		factor(n, len);
 	}
 
 	return 0;
+}
+
+void factor(mp_digit *n, mp_size len) {
+	int nfactors = 0;
+	mp_digit factor;
+	while ((factor = mp_sieve(n, len, 0)) != 0) {
+		int npowers = 0;
+
+		nfactors++;
+		do {
+			mp_digit remainder = mp_ddivi(n, len, factor);
+			ASSERT(remainder == 0);
+			len -= (n[len-1] == 0);
+			npowers++;
+		} while (mp_dmod(n, len, factor) == 0);
+		if (npowers > 1)
+			printf("Trivial factor %u^%d removed\n", factor, npowers);
+		else
+			printf("Trivial factor %u removed\n", factor);
+	}
+
+	printf("Remainder after removing %d trivial factors is:\n", nfactors);
+	mp_print_dec(n, len); printf("\n");
+
+	if (len == 1 && n[0] <= 2) {
+		printf("Trivial remainder\n");
+	} else {
+		/* Now run 10 rounds of the Rabin-Miller test. */
+		if (mp_composite(n, len, 10))
+			printf("Remainder is definitely composite.\n");
+		else
+			printf("Remainder is prime with extreme probability.\n");
+	}
 }
