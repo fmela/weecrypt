5d817db9c50a5dcd8211ca75c22727f0b0eed4e5
Farooq Mela <farooq.mela@gmail.com>
2012-07-26 14:23:14 -0700

    Use new mpi initializer functions.  Use C99 declaration style. Use output parameter as intermediate value.

--- src/mpi_fibonacci.c
+++ src/mpi_fibonacci.c
@@ -17,35 +17,37 @@
  * [ 0 1 ]  = [ F_{n-1} F_n ]
  * [ 1 1 ]    [ F_n F_{n+1} ]
  *
- * Power algorithm from high bit to low bit.
+ * Exponentiation uses binary power algorithm from high bit to low bit.
  */
 void
 mpi_fibonacci(unsigned n, mpi *fib)
 {
-	unsigned k;
-	mpi_t a0, a1, a3;
-	mpi_t a12, a03;
+	ASSERT(fib != NULL);
 
 	if (n <= 2) {
 		mpi_one(fib);
 		return;
 	}
 
-	k = 1U << 31;
-	while ((k & n) == 0)
+	/* Scan for highest set bit. TODO: change to binary search. */
+	unsigned k = 1U << 31;
+	while (!(k & n))
 		k >>= 1;
 
-	mpi_init_ui(a0, 1);				/*  a0 = 1 */
-	mpi_init(a1);					/*  a1 = 0 */
-	mpi_init_ui(a3, 1);				/*  a3 = 1 */
+	mpi *a1 = fib;					/* Use output param fib as a1 */
+
+	mpi_t a0, a3, a12, a03;
+	mpi_init_u32(a0, 1);			/*  a0 = 1 */
+	mpi_set_u32(a1, 0);				/*  a1 = 0 */
+	mpi_init_u32(a3, 1);			/*  a3 = 1 */
 	mpi_init(a12);					/* a12 = 0 */
 	mpi_init(a03);					/* a03 = 0 */
 
 	do {
 		mpi_sqr(a1, a12);			/* a12 = a1^2 */
 		mpi_add(a0, a3, a03);		/* a03 = a0 + a3 */
-		mpi_sqr(a0, a0);			/*  a0 *= a0 */
-		mpi_add(a0, a12, a0);		/*  a0 += a12 */
+		mpi_sqr(a0, a0);			/*  a0 = a0^2 */
+		mpi_add(a0, a12, a0);		/*  a0 += a12 (a0^2 + a1^2) */
 		mpi_mul(a1, a03, a1);		/*  a1 *= a03 */
 		mpi_add(a0, a1, a3);		/*  a3 = a0 * a1 */
 		if (k & n) {
@@ -55,10 +57,9 @@ mpi_fibonacci(unsigned n, mpi *fib)
 		}
 	} while (k >>= 1);
 
-	mpi_set_mpi(fib, a1);			/* F[n] = a1 */
+	/* Now a1 (alias of output parameter fib) = F[n] */
 
 	mpi_free(a0);
-	mpi_free(a1);
 	mpi_free(a3);
 	mpi_free(a12);
 	mpi_free(a03);
