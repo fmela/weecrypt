28009229f2f1a651facf577c2dd1c210559957dd
Farooq Mela <farooq.mela@gmail.com>
2012-07-26 15:20:30 -0700

    Cleanup.

--- fibonacci.c
+++ fibonacci.c
@@ -14,14 +14,15 @@ main(void)
 	while (printf("Enter N: ") &&
 		   fgets(buf, sizeof(buf), stdin)) {
 		unsigned n = (unsigned)strtoul(buf, NULL, 10);
-		if (n == 0)
+		if (!n)
 			break;
 
-		printf("You entered %u\n", n);
 		mpi_fibonacci(n, fib);
 		printf("F(%u)=", n), mpi_print_dec(fib), printf("\n");
 		printf("As double: %g\n", mpi_get_d(fib));
 	}
+
+	mpi_free(fib);
 
 	return 0;
 }
