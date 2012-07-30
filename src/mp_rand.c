bae9b7f06c82b2a5b14582279e435cf3c863f839
Farooq Mela <farooq.mela@gmail.com>
2012-07-30 15:36:47 -0700

    Declare variables as needed, C99 style. Optimize mp_rand_digits for each value of MP_DIGIT_SIZE.

--- src/mp_rand.c
+++ src/mp_rand.c
@@ -17,34 +17,29 @@
 #define MT_MATRIX_A		0x9908b0dfUL /* constant vector a */
 #define MT_UPPER_MASK	0x80000000UL /* most significant w-r bits */
 #define MT_LOWER_MASK	0x7fffffffUL /* least significant r bits */
-// #define MT_DEF_SEED		0x87654321UL
-#define MT_DEF_SEED		(unsigned)time(0)
+/* #define MT_DEFAULT_SEED	0x87654321UL */
+#define MT_DEFAULT_SEED	(unsigned)time(0)
 
 static void
 mt_init32(mp_rand_ctx *ctx, uint32_t s)
 {
-	int mti;
-
     ctx->mt[0] = s;
-    for (mti = 1; mti < MT_N; mti++)
+    for (int mti = 1; mti < MT_N; mti++)
         ctx->mt[mti] =
 			(1812433253UL * (ctx->mt[mti-1] ^ (ctx->mt[mti-1] >> 30)) + mti);
-	ctx->mti = mti;
+	ctx->mti = MT_N;
 }
 
 static uint32_t
 mt_rand32(mp_rand_ctx *ctx)
 {
-	int mti;
     uint32_t y, mag01[2] = { 0, MT_MATRIX_A };
 
-	mti = ctx->mti;
-    if (mti >= MT_N) { /* generate MT_N words at one time */
-        int k;
+    if (ctx->mti >= MT_N) { /* generate MT_N words at one time */
+        if (ctx->mti == MT_N + 1)   /* if mt_init32() has not been called, */
+            mt_init32(ctx, MT_DEFAULT_SEED); /* use default initial seed */
 
-        if (mti == MT_N + 1)   /* if mt_init32() has not been called, */
-            mt_init32(ctx, MT_DEF_SEED); /* a default initial seed is used */
-
+		int k;
         for (k = 0; k < MT_N - MT_M; k++) {
             y = (ctx->mt[k] & MT_UPPER_MASK) |
 				(ctx->mt[k + 1] & MT_LOWER_MASK);
@@ -59,11 +54,10 @@ mt_rand32(mp_rand_ctx *ctx)
 			(ctx->mt[0] & MT_LOWER_MASK);
         ctx->mt[MT_N - 1] = ctx->mt[MT_M - 1] ^ (y >> 1) ^ mag01[y & 1];
 
-        mti = 0;
+        ctx->mti = 0;
     }
 
-    y = ctx->mt[mti];
-	ctx->mti = ++mti;
+    y = ctx->mt[ctx->mti++];
 
     /* Tempering */
     y ^= (y >> 11);
@@ -79,7 +73,7 @@ mp_rand_ctx_init(mp_rand_ctx *ctx)
 {
 	ASSERT(ctx != NULL);
 
-	mt_init32(ctx, MT_DEF_SEED);
+	mt_init32(ctx, MT_DEFAULT_SEED);
 }
 
 void
@@ -102,29 +96,48 @@ mp_rand_ctx_clear(mp_rand_ctx *ctx)
 void
 mp_rand_digits(mp_rand_ctx *ctx, mp_digit *u, mp_size size)
 {
-	unsigned char *ptr;
 	static mp_rand_ctx stat_ctx = { MT_N + 1, { 0 } };
-	uint32_t k;
-	mp_size l;
 
 	if (ctx == NULL)
 		ctx = &stat_ctx;
 
-	ptr = (unsigned char *)u;
-	size *= MP_DIGIT_SIZE;
-	l = size / 4;
-	while (l--) {
-		k = mt_rand32(ctx);
-		*ptr++ = (mp_digit)k; k >>= 8;
-		*ptr++ = (mp_digit)k; k >>= 8;
-		*ptr++ = (mp_digit)k; k >>= 8;
-		*ptr++ = (mp_digit)k;
+#if MP_DIGIT_SIZE == 1
+	while (size >= 4) {
+		uint32_t r = mt_rand32(ctx);
+		*u++ = r;
+		*u++ = r >> 8;
+		*u++ = r >> 16;
+		*u++ = r >> 24;
+		size -= 4;
 	}
-	if (size &= 3) {
-		k = mt_rand32(ctx);
+	if (size) {
+		uint32_t r = mt_rand32(ctx);
 		do {
-			*ptr++ = (mp_digit)k;
-			k >>= 8;
+			*u++ = r;
+			r >>= 8;
 		} while (--size);
 	}
+#elif MP_DIGIT_SIZE == 2
+	while (size >= 2) {
+		uint32_t r = mt_rand32(ctx);
+		*u++ = r;
+		*u++ = r >> 16;
+		size -= 2;
+	}
+	if (size) {
+		*u = (mp_digit)mt_rand32(ctx);
+	}
+#elif MP_DIGIT_SIZE == 4
+	while (size--) {
+		*u++ = mt_rand32(ctx);
+	}
+#elif MP_DIGIT_SIZE == 8
+	while (size--) {
+		uint64_t r = mt_rand32(ctx);
+		r = (r << 32) | mt_rand32(ctx);
+		*u++ = r;
+	}
+#else
+# error Bad or unknown MP_DIGIT_SIZE
+#endif
 }
