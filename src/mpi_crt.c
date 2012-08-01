b607a24998a346246d26fdc7776a1973aa3532a6
Farooq Mela <farooq.mela@gmail.com>
2012-08-01 12:12:40 -0700

    Remove confusing preprocessor definitions.

--- src/mpi_crt.c
+++ src/mpi_crt.c
@@ -7,9 +7,6 @@
 
 #include "mpi.h"
 
-#define X	(ctx->x)
-#define M	(ctx->m)
-
 void
 mpi_crt_init(mpi_crt_ctx *ctx)
 {
@@ -20,8 +17,8 @@ int
 mpi_crt_step(mpi_crt_ctx *ctx, const mpi *a_i, const mpi *m_i)
 {
 	if (ctx->i == 0) {
-		mpi_init_mpi(X, a_i);
-		mpi_init_mpi(M, m_i);
+		mpi_init_mpi(ctx->x, a_i);
+		mpi_init_mpi(ctx->m, m_i);
 	} else {
 		mpi_t u, v, gcd;
 
@@ -29,24 +26,24 @@ mpi_crt_step(mpi_crt_ctx *ctx, const mpi *a_i, const mpi *m_i)
 		mpi_init(v);
 		mpi_init(gcd);
 
-		mpi_gcdext(M, m_i, u, v, gcd);
+		mpi_gcdext(ctx->m, m_i, u, v, gcd);
 		if (!mpi_is_one(gcd)) {
 			mpi_free(u);
 			mpi_free(v);
 			mpi_free(gcd);
-			mpi_free(X);
-			mpi_free(M);
+			mpi_free(ctx->x);
+			mpi_free(ctx->m);
 			ctx->i = 0;
 			return -1;
 		}
 
-		mpi_mul(u, M, u);
+		mpi_mul(u, ctx->m, u);
 		mpi_mul(u, a_i, u);
 		mpi_mul(v, m_i, v);
-		mpi_mul(v, X, v);
-		mpi_add(u, v, X);
-		mpi_mul(M, m_i, M);
-		mpi_mod(X, M, X);
+		mpi_mul(v, ctx->x, v);
+		mpi_add(u, v, ctx->x);
+		mpi_mul(ctx->m, m_i, ctx->m);
+		mpi_mod(ctx->x, ctx->m, ctx->x);
 
 		mpi_free(u);
 		mpi_free(v);
@@ -61,11 +58,11 @@ mpi_crt_finish(mpi_crt_ctx *ctx, mpi *a)
 {
 	if (ctx->i == 0)
 		return -1;
-	if (mpi_is_neg(X))
-		mpi_add(X, M, a);
+	if (mpi_is_neg(ctx->x))
+		mpi_add(ctx->x, ctx->m, a);
 	else
-		mpi_set_mpi(a, X);
-	mpi_free(X);
-	mpi_free(M);
+		mpi_set_mpi(a, ctx->x);
+	mpi_free(ctx->x);
+	mpi_free(ctx->m);
 	return 0;
 }
