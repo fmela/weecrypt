7f9d1b13b1ec000187dc681410e37648b81c011e
Farooq Mela <farooq.mela@gmail.com>
2012-08-02 16:24:12 -0700

    Leverage improved ASSERT macro. Declare variables where needed, C99 style.

--- src/mp_divrem.c
+++ src/mp_divrem.c
@@ -12,19 +12,17 @@ void
 mp_norm_div(mp_digit *u, mp_size usize,
 			const mp_digit *v, mp_size vsize, mp_digit *q)
 {
-	mp_size j;
-	mp_digit qhat, vd, borrow;
-	mp_digit *u_j;
-
 	/* Knuth's 4.3.1-D, vol.2, 3rd ed, pp.270-275 */
 
 	ASSERT(vsize >= 2);
-	vd = v[vsize - 1];
+	mp_digit vd = v[vsize - 1];
 	ASSERT((vd & MP_DIGIT_MSB) != 0);
 
 	/* D2: Initialize j. */
-	u_j = &u[j = usize - vsize]; /* u_j will point to u[j] throughout loop. */
+	mp_size j = usize - vsize;
+	mp_digit *u_j = &u[j]; /* u_j will point to u[j] throughout loop. */
 	do {
+		mp_digit qhat;
 		/* D3: Calculate qhat. */
 		if (u_j[vsize] == vd) {
 			qhat = ~(mp_digit)0;	/* largest value for mp_digit */
@@ -37,7 +35,7 @@ mp_norm_div(mp_digit *u, mp_size usize,
 			digit_mul(qhat, v[vsize - 2], q1, q0);
 			while ((q1 > rhat) ||
 				   (q1 == rhat && q0 > u_j[vsize - 2])) {
-				qhat--;
+				--qhat;
 				if ((rhat += vd) < vd)
 					break;
 				qq = q0;
@@ -45,17 +43,17 @@ mp_norm_div(mp_digit *u, mp_size usize,
 			}
 		}
 		/* D4: Multiply and subtract. */
-		borrow = mp_dmul_sub(v, vsize, qhat, u_j);
+		mp_digit borrow = mp_dmul_sub(v, vsize, qhat, u_j);
 		/* D5: Test remainder. */
 		if (u_j[vsize] < borrow) {
 			/* D6: Add back. */
-			qhat--;
+			--qhat;
 			mp_addi_n(u_j, v, vsize);
 		}
 		/* D7: Loop on j. */
 		if (q != NULL)
 			q[j] = qhat;
-		u_j--;
+		--u_j;
 	} while (j-- != 0);
 }
 
@@ -65,17 +63,13 @@ void
 mp_divrem(const mp_digit *u, mp_size usize,
 		  const mp_digit *v, mp_size vsize, mp_digit *q, mp_digit *r)
 {
-	int rv;
-	mp_digit scale, cy, *utmp;
-
-	ASSERT(u != NULL);
-	ASSERT(usize != 0);
-//	ASSERT(u[usize - 1] != 0);
-	ASSERT(v != NULL);
-	ASSERT(vsize != 0);
+	ASSERT(u);
+	ASSERT(usize > 0);
+	ASSERT(u[usize - 1] != 0);
+	ASSERT(v);
+	ASSERT(vsize > 0);
 	ASSERT(v[vsize - 1] != 0);
-//	ASSERT(usize >= vsize);
-
+	ASSERT(usize >= vsize);
 
 	if (!q && !r) /* Nothing to do. */
 		return;
@@ -93,13 +87,13 @@ mp_divrem(const mp_digit *u, mp_size usize,
 	}
 
 	/* Compare numbers. */
-	rv = mp_cmp(u, usize, v, vsize);
-	if (rv == 0) {
+	const int cmp = mp_cmp(u, usize, v, vsize);
+	if (cmp == 0) {
 		/* If equal, set quotient to 1; finished. */
 		if (q != NULL)
 			q[0] = 1;
 		return;
-	} else if (rv < 0) {
+	} else if (cmp < 0) {
 		/* If U < V, set remainder to U; finished. */
 		if (r != NULL)
 			mp_copy(u, usize, r);
@@ -109,9 +103,9 @@ mp_divrem(const mp_digit *u, mp_size usize,
 	if (vsize == 1) {
 		/* If VLEN == 1 we can divide in linear time (see exercise 16). */
 		if (q != NULL) {
-			cy = mp_ddiv(u, usize, v[0], q);
+			mp_digit remainder = mp_ddiv(u, usize, v[0], q);
 			if (r != NULL)
-				r[0] = cy;
+				r[0] = remainder;
 		} else {
 			ASSERT(r != NULL);
 			r[0] = mp_dmod(u, usize, v[0]);
@@ -122,8 +116,9 @@ mp_divrem(const mp_digit *u, mp_size usize,
 
 	/* D1: Normalize. */
 	/* Find number of leading zero bits in most significant digit of V. */
-	scale = mp_digit_msb_shift(v[vsize - 1]);
+	unsigned scale = mp_digit_msb_shift(v[vsize - 1]);
 	/* Allocate space for U << SCALE. */
+	mp_digit *utmp;
 	MP_TMP_ALLOC(utmp, usize + 1);
 	mp_copy(u, usize, utmp);
 	if (scale == 0) {
@@ -132,17 +127,13 @@ mp_divrem(const mp_digit *u, mp_size usize,
 		/* D2 - D7. */
 		mp_norm_div(utmp, usize, v, vsize, q);
 	} else {
-		mp_digit *vp;
-
-		/* XXX Assume V is writable. */
-		vp = (mp_digit *)v;
+		/* XXX Assumes V is writable. */
 		/* Shift them left SCALE positions; the size of V will not change. */
 		utmp[usize] = mp_lshifti(utmp, usize, scale);
-		cy = mp_lshifti(vp, vsize, scale);
-		ASSERT(cy == 0);
+		ASSERT(mp_lshifti((mp_digit *)v, vsize, scale) == 0);
 		/* D2 - D7. */
-		mp_norm_div(utmp, usize, vp, vsize, q);
-		mp_rshifti(vp, vsize, scale);
+		mp_norm_div(utmp, usize, v, vsize, q);
+		ASSERT(mp_rshifti((mp_digit *)v, vsize, scale) == 0);
 	}
 
 	/* D8: Store remainder. */
