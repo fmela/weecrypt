c04eb9a1d711413b06cf4b2f1a1c3715d088b175
Farooq Mela <farooq.mela@gmail.com>
2012-08-02 11:47:40 -0700

    Optimization: shift out common factors of two now and again.

---
+++ src/mpi_binomial.c
@@ -0,0 +1,48 @@
+/*
+ * mpi_binomial.c
+ * Copyright (C) 2012 Farooq Mela. All rights reserved.
+ */
+
+#include "mpi.h"
+#include "mpi_defs.h"
+
+/* Compute binomial coefficient N choose K. */
+void
+mpi_binomial(uint64_t n, uint64_t k, mpi *coeff)
+{
+	ASSERT(coeff != NULL);
+
+	/* Trivial cases */
+	if (k > n) {
+		mpi_zero(coeff);
+		return;
+	} else if (k == 0 || k == n) {
+		mpi_one(coeff);
+		return;
+	} else if (k == 1 || k == n-1) {
+		mpi_set_u64(coeff, n);
+		return;
+	}
+
+	/*             (N-K+1) x (N-K+2) x ... x N
+	  N choose K = ---------------------------
+	                     1 x 2 x ... x K       */
+	mpi_t num, den;
+	mpi_init_u64(num, n - k + 1);
+	mpi_init_u32(den, 1);
+	for (uint64_t i = 2; i <= k; ++i) {
+		mpi_mul_u64(num, n - k + i, num);
+		mpi_mul_u64(den, i, den);
+		if ((i & 31) == 0) {
+			unsigned shift = mp_digit_lsb_shift(num->digits[0] |
+												den->digits[0]);
+			if (shift) {
+				mpi_rshift(num, shift, num);
+				mpi_rshift(den, shift, den);
+			}
+		}
+	}
+	mpi_divexact(num, den, coeff);
+	mpi_free(num);
+	mpi_free(den);
+}
