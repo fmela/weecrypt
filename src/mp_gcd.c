2fcb46dd055488bf958a84226b5970c895e1ad40
Farooq Mela <farooq.mela@gmail.com>
2012-08-02 14:40:15 -0700

    Use simplified algorithm in mp_gcd, and optimize temporary storage. Expose internal single-digit GCD function as mp_digit_gcd.

--- src/mp_gcd.c
+++ src/mp_gcd.c
@@ -8,33 +8,32 @@
 #include "mp.h"
 #include "mp_defs.h"
 
-static mp_digit
-dgcd(mp_digit u, mp_digit v)
+mp_digit
+mp_digit_gcd(mp_digit u, mp_digit v)
 {
-	ASSERT(u > 0);
-	ASSERT(v > 0);
+	if (!u || !v)
+		return 0;
 
-	const unsigned ushift = mp_digit_lsb_shift(u);
-	const unsigned vshift = mp_digit_lsb_shift(v);
-	u >>= ushift;
-	v >>= vshift;
+	const unsigned shift = mp_digit_lsb_shift(u | v);
+	u >>= mp_digit_lsb_shift(u);
+	v >>= mp_digit_lsb_shift(v);
 	while (u != v) {
 		if (u > v) {
-			for (u = (u - v) >> 1; (u & 1) == 0; u >>= 1)
-				/* void */;
+			u -= v;
+			u >>= mp_digit_lsb_shift(u);
 		} else {
-			for (v = (v - u) >> 1; (v & 1) == 0; v >>= 1)
-				/* void */;
+			v -= u;
+			v >>= mp_digit_lsb_shift(v);
 		}
 	}
-	return u << MIN(ushift, vshift);
+	return u << shift;
 }
 
 /* Binary GCD algorithm, as given by Knuth (4.5.2B, vol.2, 3rd ed. p.338)
  *
  * For natural numbers U and V:
  * B1. [Find power of 2] Set K = 0, and then repeatedly set K = K + 1,
- *     V = V / 2, U = U / 2 until U and V are not both even.
+ *     V = V / 2, U = U / 2 until either U or V (or both) are odd.
  * B2. [Initialize] If U is odd, set T = -V and goto B4. Otherwise, set T = U.
  * B3. [Halve T] T = T / 2.
  * B4. [T Even?] If T is even, goto B3.
@@ -49,149 +48,110 @@ void
 mp_gcd(const mp_digit *u, mp_size usize,
 	   const mp_digit *v, mp_size vsize, mp_digit *w)
 {
-	mp_zero(w, MIN(usize, vsize));
+	const mp_size result_size = MIN(usize, vsize);
+	mp_zero(w, result_size);
 
 	MP_NORMALIZE(u, usize);
 	MP_NORMALIZE(v, vsize);
 	if (!usize || !vsize)
-		return; /* Don't handle these. */
+		return; /* Return 0. */
 
 	if (usize == 1 || vsize == 1) {
-		mp_digit a, b, r;
-
 		if (usize == 1 && vsize == 1) {
-			a = u[0]; b = v[0];
+			w[0] = mp_digit_gcd(u[0], v[0]);
 		} else {
 			if (usize == 1) {
-				if ((r = mp_dmod(v, vsize, u[0])) == 0) {
-					w[0] = u[0];
-					return;
-				}
-				a = u[0];
+				SWAP(u, v, const mp_digit *);
+				SWAP(usize, vsize, mp_size);
+			}
+			ASSERT(vsize == 1);
+			mp_digit rem = mp_dmod(u, usize, v[0]);
+			if (rem == 0) {
+				w[0] = v[0];
 			} else {
-				if ((r = mp_dmod(u, usize, v[0])) == 0) {
-					w[0] = v[0];
-					return;
-				}
-				a = v[0];
+				w[0] = mp_digit_gcd(v[0], rem);
 			}
-			b = r;
 		}
-
-#if 0
-		/* Classic Euclidean GCD algorithm; replace with single-precision
-		 * binary method? */
-		for (;;) {
-			if ((r = a % b) == 0) {
-				w[0] = b;
-				return;
-			}
-			a = b;
-			b = r;
-		}
-#endif
-		w[0] = dgcd(a, b);
 		return;
 	}
 
-	/* B1: */
-	unsigned k_skip = 0;
-	while ((u[k_skip] | v[k_skip]) == 0)
-		k_skip++;
-	u += k_skip; usize -= k_skip;
-	v += k_skip; vsize -= k_skip;
-	w += k_skip;
-	const unsigned k_shift = mp_digit_lsb_shift(u[0] | v[0]);
+	if ((u[0] | v[0]) == 0) {
+		unsigned digit_skip = 1;
+		while ((u[digit_skip] | v[digit_skip]) == 0)
+			++digit_skip;
+		u += digit_skip; usize -= digit_skip;
+		v += digit_skip; vsize -= digit_skip;
+		w += digit_skip;
+	}
+	const unsigned digit_shift = mp_digit_lsb_shift(u[0] | v[0]);
 
-	const mp_size size = MAX(usize, vsize);
-	mp_digit *utmp, *vtmp;
-	MP_TMP_ALLOC0(utmp, size);
-	MP_TMP_ALLOC0(vtmp, size);
-	if (k_shift) {
-		mp_digit cy = mp_rshift(u, usize, k_shift, utmp);
-		ASSERT(cy == 0);
-		cy = mp_rshift(v, vsize, k_shift, vtmp);
-		ASSERT(cy == 0);
+	while (u[0] == 0)
+		++u, --usize;
+	while (v[0] == 0)
+		++v, --vsize;
+	mp_digit *tmp = NULL, *utmp, *vtmp;
+	if (usize <= result_size) {
+		utmp = w;
+		MP_TMP_ALLOC(tmp, vsize);
+		vtmp = tmp;
+	} else {
+		ASSERT(vsize <= result_size);
+		vtmp = w;
+		MP_TMP_ALLOC(tmp, usize);
+		utmp = tmp;
+	}
+
+	unsigned shift = mp_digit_lsb_shift(u[0]);
+	if (shift) {
+		ASSERT(mp_rshift(u, usize, shift, utmp) == 0);
+		usize -= (utmp[usize - 1] == 0);
 	} else {
 		mp_copy(u, usize, utmp);
+	}
+	shift = mp_digit_lsb_shift(v[0]);
+	if (shift) {
+		ASSERT(mp_rshift(v, vsize, shift, vtmp) == 0);
+		vsize -= (vtmp[vsize - 1] == 0);
+	} else {
 		mp_copy(v, vsize, vtmp);
 	}
-	mp_zero(utmp + usize, size - usize);
-	mp_zero(vtmp + vsize, size - vsize);
 
-	mp_digit *T = NULL;
-	int tneg = 0;
-	mp_size tsize;
-	MP_TMP_ALLOC0(T, size);
-	/* B2: */
-	if (utmp[0] & 1) {
-		mp_copy(vtmp, vsize, T);
-		tsize = vsize;
-		tneg = 1;
-	} else {
-		mp_copy(utmp, usize, T);
-		tsize = usize;
-	}
-
-B3:	/* Combined with B4. */
-	if (!(T[0] & 1)) {	/* T is even. */
-		unsigned shift = mp_odd_shift(T, tsize);
-		unsigned digits = shift / MP_DIGIT_BITS;
-		shift %= MP_DIGIT_BITS;
-		if (digits) {
-			mp_digit cy = mp_rshift(T + digits, tsize, shift, T);
-			ASSERT(cy == 0);
-			tsize -= digits;
+	for (;;) {
+		const int cmp = mp_cmp(utmp, usize, vtmp, vsize);
+		if (cmp > 0) {
+			ASSERT(mp_subi(utmp, usize, vtmp, vsize) == 0);
+			MP_NORMALIZE(utmp, usize);
+			while (*utmp == 0)
+				++utmp, --usize;
+			shift = mp_digit_lsb_shift(utmp[0]);
+			if (shift) {
+				ASSERT(mp_rshifti(utmp, usize, shift) == 0);
+				usize -= (utmp[usize - 1] == 0);
+			}
+		} else if (cmp < 0) {
+			ASSERT(mp_subi(vtmp, vsize, utmp, usize) == 0);
+			MP_NORMALIZE(vtmp, vsize);
+			while (*vtmp == 0)
+				++vtmp, --vsize;
+			shift = mp_digit_lsb_shift(vtmp[0]);
+			if (shift) {
+				ASSERT(mp_rshifti(vtmp, vsize, shift) == 0);
+				vsize -= (vtmp[vsize - 1] == 0);
+			}
 		} else {
-			mp_digit cy = mp_rshifti(T, tsize, shift);
-			ASSERT(cy == 0);
+			ASSERT(cmp == 0);
+			if (digit_shift) {
+				mp_digit cy = mp_lshift(utmp, usize, digit_shift, w);
+				if (cy)
+					w[usize] = cy;
+			} else {
+				if (utmp != w)
+					mp_copy(utmp, usize, w);
+			}
+			break;
 		}
-		tsize -= (T[tsize - 1] == 0);
-		ASSERT(tsize > 0);
 	}
-
-	/* B5: */
-	if (tneg) {
-		mp_copy(T, tsize, vtmp);
-		vsize = tsize;
-	} else {
-		mp_copy(T, tsize, utmp);
-		usize = tsize;
-	}
-
-	MP_NORMALIZE(utmp, usize);
-	MP_NORMALIZE(vtmp, vsize);
-	int cmp = mp_cmp(utmp, usize, vtmp, vsize);
-	if (cmp > 0) {
-		tsize = usize;
-		cmp = mp_sub(utmp, usize, vtmp, vsize, T);
-		ASSERT(cmp == 0);
-		tneg = 0;
-	} else if (cmp < 0) {
-		tsize = vsize;
-		cmp = mp_sub(vtmp, vsize, utmp, usize, T);
-		ASSERT(cmp == 0);
-		tneg = 1;
-	} else if (cmp == 0) {
-		if (k_shift) {
-			mp_digit cy = mp_lshift(utmp, usize, k_shift, w);
-			/* Do NOT remove this test for cy != 0. If U and V are of the same
-			 * size then W is the same size, and we cannot write past the
-			 * ULEN-th digit. */
-			if (cy)
-				w[usize] = cy;
-		} else {
-			mp_copy(utmp, usize, w);
-		}
-		MP_TMP_FREE(utmp);
-		MP_TMP_FREE(vtmp);
-		MP_TMP_FREE(T);
-		return;
-	}
-
-	MP_NORMALIZE(T, tsize);
-	ASSERT(tsize != 0);
-	goto B3;
+	MP_TMP_FREE(tmp);
 }
 
 bool
