7b9b14eaa701df1ea505fe9c4b46a38c1d652650
Farooq Mela <farooq.mela@gmail.com>
2012-08-02 15:30:55 -0700

    Change ASSERT macro to always execute expression, regardless of NDEBUG. Update C versions of digit_mul and digit_div macros. Remove useless TRUE and FALSE definitions. Update GCC attributes.

--- include/mp_defs.h
+++ include/mp_defs.h
@@ -10,8 +10,8 @@
 
 #include "mp_config.h"
 
-#if (defined(i386) || defined(__i386__))
-# if MP_DIGIT_SIZE == 1
+#if MP_DIGIT_SIZE == 1
+# if defined(i386) || defined(__i386__)
 #  define digit_mul(u,v,hi,lo)												\
 	__asm__("mulb %3"														\
 			: "=a"(lo), "=a"(hi)											\
@@ -21,20 +21,23 @@
 	__asm__("divb %4"														\
 			: "=a"(q), "=a"(r)												\
 			: "0"(n0), "1"(n1),	"g"(d))
+# else
 #  define digit_mul(u,v,hi,lo)												\
 	do {																	\
-		mp16_t __p = (mp16_t)(u) * (mp16_t)(v);								\
-		(lo) = (mp8_t)__p;													\
-		(hi) = (mp8_t)(__p >> 8);											\
+		uint16_t __p = (uint16_t)(u) * (uint16_t)(v);						\
+		(lo) = (mp_digit)__p;												\
+		(hi) = (mp_digit)(__p >> 8);										\
 	} while (0)
 #  define digit_div(n1,n0,d,q,r)											\
 	do {																	\
-		mp8_t __d = (d);													\
-		mp16_t __n = ((mp16_t)(n1) << 8) | (mp16_t)(n0);					\
-		(q) = (mp8_t)(__n / __d);											\
-		(r) = (mp8_t)(__n % __d);											\
+		uint16_t __d = (d);													\
+		uint16_t __n = ((uint16_t)(n1) << 8) | (uint16_t)(n0);				\
+		(q) = (mp_digit)(__n / __d);										\
+		(r) = (mp_digit)(__n % __d);										\
 	} while (0)
-# elif MP_DIGIT_SIZE == 2
+# endif
+#elif MP_DIGIT_SIZE == 2
+# if defined(i386) || defined(__i386__)
 #  define digit_mul(u,v,hi,lo)												\
 	__asm__("mulw %3"														\
 			: "=a"(lo), "=d"(hi)											\
@@ -44,7 +47,23 @@
 	__asm__("divw %4"														\
 			: "=a"(q), "=d"(r)												\
 			: "0"(n0), "1"(n1), "g"(d))
-# elif MP_DIGIT_SIZE == 4
+# else
+#  define digit_mul(u,v,hi,lo)												\
+	do {																	\
+		uint32_t __p = (uint32_t)(u) * (uint32_t)(v);						\
+		(lo) = (mp_digit)__p;												\
+		(hi) = (mp_digit)(__p >> 16);										\
+	} while (0)
+#  define digit_div(n1,n0,d,q,r)											\
+	do {																	\
+		uint32_t __d = (d);													\
+		uint32_t __n = ((uint32_t)(n1) << 16) | (uint32_t)(n0);				\
+		(q) = (mp_digit)(__n / __d);										\
+		(r) = (mp_digit)(__n % __d);										\
+	} while (0)
+# endif
+#elif MP_DIGIT_SIZE == 4
+# if defined(i386) || defined(__i386__)
 #  define digit_mul(u,v,hi,lo)												\
 	__asm__("mull %3"														\
 			: "=a"(lo), "=d"(hi)											\
@@ -55,15 +74,17 @@
 			: "=a"(q), "=d"(r)												\
 			: "0"(n0), "1"(n1),	"g"(d))
 # endif
-#elif MP_DIGIT_SIZE == 8 && (defined(__amd64__) || defined(__x86_64__))
-# define digit_mul(u,v,hi,lo)												\
+#elif MP_DIGIT_SIZE == 8
+# if defined(__amd64__) || defined(__x86_64__)
+#  define digit_mul(u,v,hi,lo)												\
 	__asm__("mulq %3"														\
 			: "=a" (lo), "=d" (hi)											\
 			: "%0" ((u)), "rm" ((v)))
-# define digit_div(n1,n0,d,q,r)												\
+#  define digit_div(n1,n0,d,q,r)											\
 	__asm__("divq %4"														\
 			: "=a" (q), "=d" (r)											\
 			: "0" ((n0)), "1" ((n1)), "rm" ((d)))
+# endif
 #endif
 
 #ifdef USE_ALLOCA
@@ -76,9 +97,9 @@
 # define MP_TMP_ALLOC(n,size)	(n) = alloca((size) * MP_DIGIT_SIZE)
 # define MP_TMP_FREE(tmp)		(void)0
 #else
-# include "mp_memory.h"
-# define MP_PTR_ALLOC(p,size)	(p) = xmalloc(size)
-# define MP_PTR_FREE(p)			free(p)
+# include "weecrypt_memory.h"
+# define MP_PTR_ALLOC(p,size)	(p) = weecrypt_xmalloc(size)
+# define MP_PTR_FREE(p)			weecrypt_xfree(p)
 # define MP_TMP_ALLOC(n,size)	(n) = mp_new(size)
 # define MP_TMP_FREE(n)			mp_free(n)
 #endif
@@ -104,9 +125,9 @@ void _mp_digit_div(mp_digit n1, mp_digit n0, mp_digit d,
  * fall back onto the (slow) C versions. */
 #ifndef digit_sqr
 # ifdef  digit_mul
-#  define digit_sqr(u,hi,lo)	digit_mul((u),(u),(hi),(lo))
+#  define digit_sqr(u, hi, lo)	digit_mul((u), (u), (hi), (lo))
 # else
-#  define digit_sqr(u,hi,lo) \
+#  define digit_sqr(u, hi, lo) \
 	do { \
 		mp_digit __u0, __u1, __hi, __lo; \
 		__u0 = (u); __u1 = __u0 >> MP_DIGIT_HSHIFT; __u0 &= MP_DIGIT_LMASK; \
@@ -146,25 +167,16 @@ void _mp_digit_div(mp_digit n1, mp_digit n0, mp_digit d,
 # define digit_div(n1,n0,d,q,r)	_mp_digit_div((n1), (n0), (d), &(q), &(r))
 #endif
 
-/* Commonly used definitions. */
-#ifndef FALSE
-#define FALSE	0
-#endif
-
-#ifndef TRUE
-#define TRUE	1
-#endif
-
 #ifndef MIN
-#define MIN(a,b)	((a) < (b) ? (a) : (b))
+# define MIN(a,b)	((a) < (b) ? (a) : (b))
 #endif
 
 #ifndef MAX
-#define MAX(a,b)	((a) > (b) ? (a) : (b))
+# define MAX(a,b)	((a) > (b) ? (a) : (b))
 #endif
 
 #ifndef SWAP
-#define SWAP(a,b,type) \
+# define SWAP(a,b,type) \
 	do { \
 		type __tmp = (a); \
 		(a) = (b); \
@@ -173,23 +185,27 @@ void _mp_digit_div(mp_digit n1, mp_digit n0, mp_digit d,
 #endif
 
 #if defined(__GNUC__)
-# define GCC_INLINE		__inline__
+# define GCC_PURE		__attribute__((__pure__))
 # define GCC_CONST		__attribute__((__const__))
+# define GCC_DEPRECATED	__attribute__((__deprecated__))
 # define GCC_UNUSED		__attribute__((__unused__))
 # define GCC_NORETURN	__attribute__((__noreturn__))
+# define GCC_MALLOC		__attribute__((__malloc__))
 #else
-# define GCC_INLINE
+# define GCC_PURE
 # define GCC_CONST
+# define GCC_DEPRECATED
 # define GCC_UNUSED
 # define GCC_NORETURN
+# define GCC_MALLOC
 #endif
 
+/* A feature (or bug) of this macro is that the expression always executes,
+ * regardless of whether NDEBUG is defined or not. This is intentional. */
 #ifndef NDEBUG
 # include <stdio.h>
 # include <stdlib.h>
-# undef ASSERT
-# if defined(__GNUC__)
-#  define ASSERT(expr)														\
+# define ASSERT(expr)														\
 	do {																	\
 		if (!(expr)) {														\
 			fprintf(stderr, "%s:%d (%s) assertion failed: \"%s\"\n",		\
@@ -197,18 +213,8 @@ void _mp_digit_div(mp_digit n1, mp_digit n0, mp_digit d,
 			abort();														\
 		}																	\
 	} while (0)
-# else
-#  define ASSERT(expr)														\
-	do {																	\
-		if (!(expr)) {														\
-			fprintf(stderr, "%s:%d assertion failed: \"%s\"\n",				\
-					__FILE__, __LINE__, #expr);								\
-			abort();														\
-		}																	\
-	} while (0)
-# endif
 #else
-# define ASSERT(expr)	(void)0
+# define ASSERT(expr)	(void)(expr)
 #endif
 
 #endif /* !_MP_DEFS_H_ */
