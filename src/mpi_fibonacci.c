358f3fb26c4f4b6560d69d783523139be688aeaf
Farooq Mela <farooq.mela@gmail.com>
2012-08-14 01:43:38 -0700

    Add another way of advancing the sequence. Performance seems to be the same between the two methods.

--- src/mpi_fibonacci.c
+++ src/mpi_fibonacci.c
@@ -37,19 +37,28 @@ mpi_fibonacci(uint64_t n, mpi *fib)
 
 	mpi *a1 = fib;					/* Use output param fib as a1 */
 
-	mpi_t a0, tmp, a03;
+	mpi_t a0, tmp, a;
 	mpi_init_u32(a0, 0);			/*  a0 = 0 */
 	mpi_set_u32(a1, 1);				/*  a1 = 1 */
 	mpi_init(tmp);					/* tmp = 0 */
-	mpi_init(a03);					/* a03 = 0 */
+	mpi_init(a);
 
 	while (k >>= 1) {
-		mpi_lshift(a0, 1, a03);		/* a03 = a0 * 2 */
-		mpi_add(a03, a1, a03);		/*   ... + a1 */
-		mpi_sqr(a1, tmp);			/* tmp = a1^2 */
+#if 1
+		mpi_lshift(a0, 1, a);       /* a03 = a0 * 2 */
+		mpi_add(a, a1, a);          /*   ... + a1 */
+		mpi_sqr(a1, tmp);           /* tmp = a1^2 */
+		mpi_sqr(a0, a0);            /* a0 = a0 * a0 */
+		mpi_add(a0, tmp, a0);       /*  ... + a1 * a1 */
+		mpi_mul(a1, a, a1);         /*  a1 = a1 * a */
+#else
+		mpi_sqr(a1, a);				/* a12 = a1 * a1 */
+		mpi_mul(a0, a1, tmp);		/* tmp = a0 * a1 */
+		mpi_lshift(tmp, 1, tmp);	/* tmp = 2 * tmp */
 		mpi_sqr(a0, a0);			/* a0 = a0 * a0 */
-		mpi_add(a0, tmp, a0);		/*    ... + a1 * a1 */
-		mpi_mul(a1, a03, a1);		/*  a1 = a1 * a03 */
+		mpi_add(a0, a, a0);			/*  ... + a1 * a1 */
+		mpi_add(tmp, a, a1);		/* a1 = a1 * a1 + tmp */
+#endif
 		if (k & n) {
 			mpi_swap(a1, a0);		/*  a1 <-> a0 */
 			mpi_add(a0, a1, a1);	/*  a1 += a0 */
@@ -60,5 +69,5 @@ mpi_fibonacci(uint64_t n, mpi *fib)
 
 	mpi_free(a0);
 	mpi_free(tmp);
-	mpi_free(a03);
+	mpi_free(a);
 }
