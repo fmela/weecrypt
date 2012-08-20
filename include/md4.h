1c96eb0ce13808cfbb0b89cca739103491828291
Farooq Mela <farooq.mela@gmail.com>
2012-08-20 16:58:04 -0700

    Update header comment, use C99 integer types, declare variables where needed.

--- include/md4.h
+++ include/md4.h
@@ -1,21 +1,20 @@
 /* md4.h
- * Copyright (C) 2000-2010 Farooq Mela. All rights reserved.
- *
- * $Id$
- */
+ * Copyright (C) 2000-2012 Farooq Mela. All rights reserved. */
 
 #ifndef _MD4_H_
 #define _MD4_H_
 
+#include <stdint.h>
+
 typedef struct {
-  unsigned int	md4[ 4];
-  unsigned int  len[ 2];	// XXX should switch to u_int64_t?
-  unsigned char	buf[64];
+  uint32_t md4[4];
+  uint32_t len[2];
+  uint8_t buf[64];
 } md4_context;
 
 void md4_init(md4_context *ctx);
 void md4_update(md4_context *ctx, const void *data, unsigned len);
 void md4_final(md4_context *ctx, void *digest);
-void md4_hash(const void *input, unsigned len, void *digest); // digest must be 16 bytes
+void md4_hash(const void *input, unsigned len, void *digest); // 16-byte output
 
 #endif // !__FX_MD4_H__
--- src/md4.c
+++ src/md4.c
@@ -1,11 +1,7 @@
 /* md4.c
- * Copyright (C) 2000-2010 Farooq Mela. All rights reserved.
+ * Copyright (C) 2000-2012 Farooq Mela. All rights reserved.
  *
- * An implementation of the Message Digest 4 algorithm. See RFC1320 for
- * algorithm details.
- *
- * $Id$
- */
+ * An implementation of Message Digest 4, as described in RFC1320. */
 
 #include "md4.h"
 
@@ -14,20 +10,20 @@
 #include "mp.h"
 #include "mp_defs.h"
 
-static void md4_step(unsigned int *md4, const unsigned char *block);
-static void md4_encode(unsigned char *output, const unsigned int *input, unsigned len);
-static void md4_decode(unsigned int *output, const unsigned char *input, unsigned len);
+static void md4_step(const uint8_t *block, uint32_t *md4);
+static void md4_encode(const uint32_t *input, unsigned len, uint8_t *output);
+static void md4_decode(const uint8_t *input, unsigned len, uint32_t *output);
 
-static unsigned char padding[64] = {
+static const uint8_t padding[64] = {
 	0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-static unsigned int A[4] = { 3, 7, 11, 19 };
-static unsigned int B[4] = { 3, 5,  9, 13 };
-static unsigned int C[4] = { 3, 9, 11, 15 };
+static const uint32_t A[4] = { 3, 7, 11, 19 };
+static const uint32_t B[4] = { 3, 5,  9, 13 };
+static const uint32_t C[4] = { 3, 9, 11, 15 };
 
 #define MD4_MAGIC0		0x67452301
 #define MD4_MAGIC1		0xefcdab89
@@ -68,32 +64,26 @@ md4_init(md4_context *ctx)
 void
 md4_update(md4_context *ctx, const void *data, unsigned len)
 {
-	const unsigned char *input;
-	unsigned i, idx, plen;
+	ASSERT(ctx != NULL);
+	ASSERT(data != NULL);
+	ASSERT(len > 0);
 
-	ASSERT(ctx!=NULL);
-	ASSERT(data!=NULL);
-	ASSERT(len>0);
-
-	input = (const unsigned char *)data;
 
 	/* # of bytes, % 64 */
-	idx = (ctx->len[0] >> 3) & 0x3f;
+	unsigned idx = (ctx->len[0] >> 3) & 0x3f;
 
 	/* number of bits */
-	ctx->len[0] += (unsigned int)len << 3;
-	if (ctx->len[0] < ((unsigned int)len << 3)) /* overflow */
-		ctx->len[1]++;
-	ctx->len[1] += (unsigned int)len >> 29; /* 29 b/c want bits not bytes */
-	plen = 64 - idx;
+	ctx->len[1] += ((ctx->len[0] += len << 3) < (len << 3));
+	ctx->len[1] += len >> 29;
+	unsigned plen = 64 - idx;
+	const uint8_t *input = data;
+	unsigned i = 0;
 	if (len >= plen) {
 		memcpy(&ctx->buf[idx], input, plen);
-		md4_step(ctx->md4, ctx->buf);
+		md4_step(ctx->buf, ctx->md4);
 		for (i = plen; i + 63 < len; i += 64)
-			md4_step(ctx->md4, &input[i]);
+			md4_step(&input[i], ctx->md4);
 		idx = 0;
-	} else {
-		i = 0;
 	}
 
 	/* buffer rest */
@@ -103,43 +93,40 @@ md4_update(md4_context *ctx, const void *data, unsigned len)
 void
 md4_final(md4_context *ctx, void *digest)
 {
-	unsigned char bits[8];
-	unsigned idx, plen;
-
 	ASSERT(ctx != NULL);
 	ASSERT(digest != NULL);
 
 	/* # of bits */
-	md4_encode(bits, ctx->len, 8);
+	uint8_t bits[8];
+	md4_encode(ctx->len, 8, bits);
 
 	/* Pad out to 56 mod 64. */
-	idx = (unsigned)((ctx->len[0] >> 3) & 0x3f);
-	plen = (idx < 56) ? (56 - idx) : (120 - idx);
+	unsigned idx = (ctx->len[0] >> 3) & 0x3f;
+	unsigned plen = (idx < 56) ? (56 - idx) : (120 - idx);
 	md4_update(ctx, padding, plen);
 
 	/* add length */
 	md4_update(ctx, bits, 8);
 
 	/* md4 */
-	md4_encode((unsigned char *)digest, ctx->md4, 16);
+	md4_encode(ctx->md4, 16, digest);
 
 	memset(ctx, 0, sizeof(md4_context));
 }
 
 static void
-md4_step(unsigned int *md4, const unsigned char *block)
+md4_step(const uint8_t *block, uint32_t *md4)
 {
-	unsigned int a, b, c, d, x[16];
+	ASSERT(md4 != NULL);
+	ASSERT(block != NULL);
 
-	ASSERT(md4!=NULL);
-	ASSERT(block!=NULL);
+	uint32_t a = md4[0];
+	uint32_t b = md4[1];
+	uint32_t c = md4[2];
+	uint32_t d = md4[3];
 
-	a = md4[0];
-	b = md4[1];
-	c = md4[2];
-	d = md4[3];
-
-	md4_decode(x, block, 64);
+	uint32_t x[16];
+	md4_decode(block, 64, x);
 
 	RND1(a, b, c, d,  0, 1);
 	RND1(d, a, b, c,  1, 2);
@@ -201,49 +188,44 @@ md4_step(unsigned int *md4, const unsigned char *block)
 }
 
 static void
-md4_encode(unsigned char *output, const unsigned int *input, unsigned len)
+md4_encode(const uint32_t *input, unsigned len, uint8_t *output)
 {
-	unsigned i, j;
-
 	ASSERT(output != NULL);
 	ASSERT(input != NULL);
 	ASSERT(len > 0);
 
-	for (i = 0, j = 0; j < len; i++, j += 4) {
-		output[j + 0] = (unsigned char)((input[i] >>  0) & 0xff);
-		output[j + 1] = (unsigned char)((input[i] >>  8) & 0xff);
-		output[j + 2] = (unsigned char)((input[i] >> 16) & 0xff);
-		output[j + 3] = (unsigned char)((input[i] >> 24) & 0xff);
+	for (unsigned i = 0, j = 0; j < len; i++, j += 4) {
+		output[j + 0] = (uint8_t)((input[i] >>  0) & 0xff);
+		output[j + 1] = (uint8_t)((input[i] >>  8) & 0xff);
+		output[j + 2] = (uint8_t)((input[i] >> 16) & 0xff);
+		output[j + 3] = (uint8_t)((input[i] >> 24) & 0xff);
 	}
 }
 
 static void
-md4_decode(unsigned int *output, const unsigned char *input, unsigned len)
+md4_decode(const uint8_t *input, unsigned len, uint32_t *output)
 {
-	unsigned i, j;
+	ASSERT(output != NULL);
+	ASSERT(input != NULL);
+	ASSERT(len > 0);
 
-	ASSERT(output!=NULL);
-	ASSERT(input!=NULL);
-	ASSERT(len>0);
-
-	for (i = 0, j = 0; j < len; i++, j += 4)
-		output[i]=
-			(((unsigned int)input[j + 0]) <<  0) |
-			(((unsigned int)input[j + 1]) <<  8) |
-			(((unsigned int)input[j + 2]) << 16) |
-			(((unsigned int)input[j + 3]) << 24);
+	for (unsigned i = 0, j = 0; j < len; i++, j += 4)
+		output[i] =
+			(((uint32_t)input[j + 0]) <<  0) |
+			(((uint32_t)input[j + 1]) <<  8) |
+			(((uint32_t)input[j + 2]) << 16) |
+			(((uint32_t)input[j + 3]) << 24);
 }
 
 
 void
 md4_hash(const void *input, unsigned len, void *digest)
 {
-	md4_context ctx;
-
 	ASSERT(input != NULL);
 	ASSERT(len > 0);
 	ASSERT(digest != NULL);
 
+	md4_context ctx;
 	md4_init(&ctx);
 	md4_update(&ctx, input, len);
 	md4_final(&ctx, digest);
