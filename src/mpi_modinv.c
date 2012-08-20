16a7d7c61ba1a372a8edacc43ed449d06bbd3f8a
Farooq Mela <farooq.mela@gmail.com>
2012-08-20 16:46:19 -0700

    Update header comment.

--- src/mpi_modinv.c
+++ src/mpi_modinv.c
@@ -1,5 +1,5 @@
 /* mpi_modinv.c
- * Copyright (C) 2003-2010 Farooq Mela. All rights reserved. */
+ * Copyright (C) 2003-2012 Farooq Mela. All rights reserved. */
 
 #include "mpi.h"
 #include "mpi_defs.h"
