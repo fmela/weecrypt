9da76c4e3d2b20e9832e78ba224bbfff36723b2d
Farooq Mela <farooq.mela@gmail.com>
2012-09-04 11:03:20 -0700

    Reformat.

--- factorial.c
+++ factorial.c
@@ -9,30 +9,30 @@ void print_factorial(unsigned n);
 int
 main(void)
 {
-	char buf[512];
-	mpi_t fib;
-	mpi_init(fib);
+    char buf[512];
+    mpi_t fib;
+    mpi_init(fib);
 
-	while (printf("Enter N: ") &&
-		   fgets(buf, sizeof(buf), stdin)) {
-		unsigned n = (unsigned)strtoul(buf, NULL, 10);
-		if (!n)
-			break;
-		print_factorial(n);
-	}
+    while (printf("Enter N: ") &&
+	   fgets(buf, sizeof(buf), stdin)) {
+	unsigned n = (unsigned)strtoul(buf, NULL, 10);
+	if (!n)
+	    break;
+	print_factorial(n);
+    }
 
-	return 0;
+    return 0;
 }
 
 void print_factorial(unsigned n)
 {
-	printf("%u! = ", n);
-	fflush(stdout);
-	mpi_t f;
-	mpi_init(f);
-	mpi_factorial(n, f);
-	mpi_print_dec(f), printf("\n");
-	printf(" As float: %g\n", mpi_get_f(f));
-	printf("As double: %g\n", mpi_get_d(f));
-	mpi_free(f);
+    printf("%u! = ", n);
+    fflush(stdout);
+    mpi_t f;
+    mpi_init(f);
+    mpi_factorial(n, f);
+    mpi_print_dec(f), printf("\n");
+    printf(" As float: %g\n", mpi_get_f(f));
+    printf("As double: %g\n", mpi_get_d(f));
+    mpi_free(f);
 }
