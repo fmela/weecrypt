139fcfb5147aecd96fdf9196eed0ceb7feffd1d6
Farooq Mela <farooq.mela@gmail.com>
2012-09-04 11:03:42 -0700

    Reformat.

--- fibonacci.c
+++ fibonacci.c
@@ -7,23 +7,23 @@
 int
 main(void)
 {
-	char buf[512];
-	mpi_t fib;
-	mpi_init(fib);
+    char buf[512];
+    mpi_t fib;
+    mpi_init(fib);
 
-	while (printf("Enter N: ") &&
-		   fgets(buf, sizeof(buf), stdin)) {
-		unsigned n = (unsigned)strtoul(buf, NULL, 10);
-		if (!n)
-			break;
+    while (printf("Enter N: ") &&
+	   fgets(buf, sizeof(buf), stdin)) {
+	unsigned n = (unsigned)strtoul(buf, NULL, 10);
+	if (!n)
+	    break;
 
-		mpi_fibonacci(n, fib);
-		printf("F(%u)=", n), mpi_print_dec(fib), printf("\n");
-		printf(" As float: %g\n", mpi_get_f(fib));
-		printf("As double: %g\n", mpi_get_d(fib));
-	}
+	mpi_fibonacci(n, fib);
+	printf("F(%u)=", n), mpi_print_dec(fib), printf("\n");
+	printf(" As float: %g\n", mpi_get_f(fib));
+	printf("As double: %g\n", mpi_get_d(fib));
+    }
 
-	mpi_free(fib);
+    mpi_free(fib);
 
-	return 0;
+    return 0;
 }
