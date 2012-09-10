314d01e0e6475355cf31d039fd37646ece73db4a
Farooq Mela <farooq.mela@gmail.com>
2012-09-10 15:47:17 -0700

    Fix internal mpi definitions.

--- include/mpi_defs.h
+++ include/mpi_defs.h
@@ -4,34 +4,31 @@
 #ifndef _MPI_DEFS_H_
 #define _MPI_DEFS_H_
 
-#define MPI_MIN_ALLOC(n,s)													\
-	do {																	\
-		mpi *__n = (n);														\
-		mp_size __s = (s);													\
-		if (__n->alloc < __s) {												\
-			__s = (__s + 3) & ~3U;											\
-			__n->digits = mp_resize(__n->digits, __s);						\
-			__n->alloc = __s;												\
-		}																	\
-	} while (0)
+#define MPI_MIN_ALLOC(n,s) \
+    do { \
+	mpi *const __n = (n); \
+	const mp_size __s = (s); \
+	if (__n->alloc < __s) { \
+	    __n->digits = mp_resize(__n->digits, \
+				    __n->alloc = ((__s + 3) & ~3U)); \
+	} \
+    } while (0)
 
-#define MPI_SIZE(n,s)														\
-	do {																	\
-		mpi *__n = (n);														\
-		mp_size __s = (s);													\
-		__n->size = __s;													\
-		if (__n->alloc < __s) {												\
-			__s = (__s + 3) & ~3U;											\
-			__n->digits = mp_resize(__n->digits, __s);						\
-			__n->alloc = __s;												\
-		}																	\
-	} while (0)
+#define MPI_SIZE(n,s) \
+    do { \
+	mpi *const __n = (n); \
+	__n->size = (s); \
+	if (__n->alloc < __n->size) { \
+	    __n->digits = mp_resize(__n->digits, \
+				    __n->alloc = ((__n->size + 3) & ~3U)); \
+	} \
+    } while (0)
 
-#define MPI_NORMALIZE(n)													\
-	do {																	\
-		mpi *__n = (n);														\
-		while (__n->size && !__n->digits[__n->size - 1])					\
-			--__n->size;													\
-	} while (0)
+#define MPI_NORMALIZE(n) \
+    do { \
+	mpi *const __n = (n); \
+	while (__n->size && !__n->digits[__n->size - 1]) \
+	    --__n->size; \
+    } while (0)
 
 #endif /* !_MPI_DEFS_H_ */
